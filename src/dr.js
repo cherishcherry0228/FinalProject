37578e50957ca742c6b528978915b6106aa69daa
cherishcherry <heorum03@naver.com>
2023-08-22 17:23:47 +0900

    css0822

--- src/dr.js
+++ src/dr.js
@@ -1,17 +1,22 @@
 // import "./styles.css";
+import React from "react";
 import { useNavigate } from "react-router-dom";
+import "./dr.css"; // 별도의 CSS 파일을 import하여 스타일링을 적용
 
 export default function Dr() {
   const movePage = useNavigate();
 
-  function arrive(){
-     movePage('/user/home');
-   }   
-  return (
+  function arrive() {
+    movePage('/user/home');
+  }
 
+  return (
     <div className="mypage">
       {/* 인식할 코드 */}
-      <button onClick={arrive}>도착</button>
-      </div>
+      <button className="start-button" onClick={arrive}>
+        도착
+      </button>
+    </div>
   );
 }
+
--- src/home.js
+++ src/home.js
@@ -1,23 +1,34 @@
-// // import "./styles.css";
+import React from "react";
 import { useNavigate } from "react-router-dom";
+import "./home.css"; // 별도의 CSS 파일을 import하여 스타일링을 적용
 
 export default function Home() {
   const movePage = useNavigate();
 
-  function drive(){
-     movePage('/user/dr');
-   }
+  function drive() {
+    movePage("/user/dr");
+  }
+
   return (
-    
-    <div className="home">
-      <br /> 
-      - 얼굴이 가운데 오도록 화면을 조정해주세요
-      <br /> 
-      - 화면에 나오는 얼굴 중 인식할 얼굴이 제일 커야해요
-      <br /> 
-      - 소리를 키워주세요
-      <br /> 
-      <button onClick={drive}>운전시작</button>
+    <div className="home-container">
+      <div className="home-content">
+        <p className="instruction">
+          오늘도 안전운행<br/> 졸음운전 잠시 쉬었다가요<br/>
+        </p>
+        <img
+          src="https://cdn-icons-png.flaticon.com/512/1464/1464630.png"
+          alt="운전 아이콘"
+          className="drive-icon"
+        />
+        <p>
+          얼굴이 가운데 오도록  <br/>화면을 조정해주세요<br />
+          <br />
+          소리를 키워주세요
+        </p>
+        <button className="start-button" onClick={drive}>
+          운전 시작
+        </button>
       </div>
+    </div>
   );
 }
